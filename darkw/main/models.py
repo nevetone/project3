7f8b5534a64b8e6b54bdbbb7c3a9353afce2b1a3
BuildTools <unconfigured@null.spigotmc.org>
2020-04-01 22:45:01 +0200

    user model

--- darkw/main/models.py
+++ darkw/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class users(models.Model):
+    nickname = models.ForeignKey(User, on_delete=models.CASCADE)
