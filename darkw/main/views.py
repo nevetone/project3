b5e45426bf28a76926af21d0b0494e0bb849547d
BuildTools <unconfigured@null.spigotmc.org>
2020-04-03 00:42:11 +0200

    register/login form - szybkie poprawi

--- darkw/main/views.py
+++ darkw/main/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    
-    
-    
-    
     template = 'index.html'
+    context = {
+
+    }
+    return render(request, template, context)
+
+def status(request):
+    template = 'status.html'
     context = {
 
     }
