650ad97140b51bba4817d9f20b24b44cecb2b81d
BuildTools <unconfigured@null.spigotmc.org>
2020-04-03 00:30:26 +0200

    register/login form

--- darkw/main/admin.py
+++ darkw/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import users
+from .models import Players, Organizations
 
 # Register your models here.
 
-admin.site.register(users)
+admin.site.register(Players)
+admin.site.register(Organizations)
--- darkw/main/models.py
+++ darkw/main/models.py
@@ -5,5 +5,24 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class users(models.Model):
+
+class Players(models.Model):
     nickname = models.ForeignKey(User, on_delete=models.CASCADE)
+    money = models.IntegerField()
+    organization_status = models.BooleanField(default=False)  
+    organization = models.ForeignKey('Organizations', on_delete=models.CASCADE, blank=True, null=True)
+    created = models.DateField(auto_now=False, auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.nickname)
+    
+
+class Organizations(models.Model):
+    boss = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    activated = models.BooleanField(default=False)
+    organization_name = models.CharField(max_length=50)
+    created = models.DateField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return str(self.organization_name) + ' | ' + str(self.boss)
+    
