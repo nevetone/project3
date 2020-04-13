d0756802ebab088a9c86ee4caf359e7faf09c9ae
BuildTools <unconfigured@null.spigotmc.org>
2020-04-13 14:40:18 +0200

    akceptowanie zlecen

--- darkw/orders/models.py
+++ darkw/orders/models.py
@@ -6,11 +6,13 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Orders(models.Model):
     player = models.ForeignKey("main.Players", on_delete=models.CASCADE, blank=True, null=True)
+    ordered_by = models.ForeignKey("main.Players", related_name='ordered_by', blank=True, null=True, on_delete=models.CASCADE)
     status = models.ForeignKey("OrdersStatus", on_delete=models.CASCADE)
     order_type = models.ForeignKey("OrdersType", on_delete=models.CASCADE)
     if_kontrabanda_or_sprzedaz = models.ManyToManyField("items.PlayerItems")
     if_pranie = models.IntegerField(default=5000 , blank=True, null=True, validators=[MaxValueValidator(30000), MinValueValidator(5000)])
     if_pranie_prowizja = models.IntegerField(blank=True, null=True, validators=[MaxValueValidator(100), MinValueValidator(1)], default=10)
+    received = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     
     class Meta:
--- darkw/orders/views.py
+++ darkw/orders/views.py
@@ -5,8 +5,9 @@ from main.models import Players
 
 def orders(request):
     template = 'orders.html'
-    
+    akcept = None
     player = None
+    
     # pobieranie gracza
     try:
         player = Players.objects.get(nickname=request.user)
@@ -22,14 +23,28 @@ def orders(request):
         player_orders = None
     try:
         status = OrdersStatus.objects.all()
-        print(status)
+        types = OrdersType.objects.all()
     except:
         status = None
+        types = None
+    try:
+        akcept = request.GET.get('akcept')
+    except:
+        akcept = None
     
+    try:
+        akcept_order = Orders.objects.get(id = int(akcept))
+        akcept_order.ordered_by = player
+        akcept_order.status = status[1]
+        akcept_order.save()
+    except:
+        pass
+    
+        
 
-
     
+            
     
-    context = {'status':status,'player':player,'orders':player_orders,'all_orders':all_orders,}
+    context = {'status':status,'types':types,'player':player,'orders':player_orders,'all_orders':all_orders,}
     return render(request, template, context)
     
